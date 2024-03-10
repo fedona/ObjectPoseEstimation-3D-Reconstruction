c58bbf65de90a9ddca4074146242d662f3561753
fedona <79803638+fedona@users.noreply.github.com>
2024-03-10 15:35:05 +0100

    Update run_my_d.sh

--- megapose6d/run_my_d.sh
+++ megapose6d/run_my_d.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Small bash script used to run megapose6d demo on multiple images from the same (converted) dataset
+# it is necessary to change the paths accordingly
+
 # Directory containing subfolders
 input="$1"
 
+# This command is necessary to set an environment variable that tells the megapose demo where to find the dataset
 export MEGAPOSE_DATA_DIR="/local/home/fedona/Desktop/ETH/Bachelor/Datasets/dst_for_megapose/$input"
 
-
+# This is the location of the folder
 base_dir="/local/home/fedona/Desktop/ETH/Bachelor/Datasets/dst_for_megapose/$input/examples/"
 
 # Iterate over subfolders
